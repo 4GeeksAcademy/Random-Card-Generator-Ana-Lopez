ccc03b29ab5c17c2b4d1dcb3ee4bcc2540680365
Ana Lopez <avantadesignlab@gmail.com>
2026-03-05 15:53:21 +0000

    añadiendo el boton

--- src/app.js
+++ src/app.js
@@ -5,11 +5,11 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-let palos = ["♦", "♥", "♠", "♣"]
-let numeros = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J", "A"];
-let azar = Math.floor(Math.random() * palos.length)
-let azar2 = Math.floor(Math.random() * numeros.length)
+window.onload = function () {
+  let palos = ["♦", "♥", "♠", "♣"]
+  let numeros = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J", "A"];
+  let azar = Math.floor(Math.random() * palos.length)
+  let azar2 = Math.floor(Math.random() * numeros.length)
 
 
   // document.querySelector("#card").innerHTML = (numeros[azar2]+ palos[azar]);
@@ -17,12 +17,34 @@ let azar2 = Math.floor(Math.random() * numeros.length)
   palosRenderizados[0].innerHTML = (palos[azar]);
   palosRenderizados[1].innerHTML = (palos[azar]);
   document.querySelector(".numero").innerHTML = (numeros[azar2]);
+  let boton = document.querySelector(".updatecard");
 
   palosRenderizados[1].style.transform = "rotate(180deg)";
 
-  if (palos[azar] === "♥"){
-    palosRenderizados[0].style.color= "red"
-  palosRenderizados[1].style.color="red"
+  if (palos[azar] === "♥") {
+    palosRenderizados[0].style.color = "red"
+    palosRenderizados[1].style.color = "red"
+
   }
-  
+
+
+  boton.addEventListener("click", function () {
+
+    let palos = ["♦", "♥", "♠", "♣"]
+    let numeros = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J", "A"];
+    let azar = Math.floor(Math.random() * palos.length)
+    let azar2 = Math.floor(Math.random() * numeros.length)
+
+    if (palos[azar] === "♥"){
+    palosRenderizados[0].style.color= "red"
+    palosRenderizados[1].style.color="red"
+
+  }
+
+    let palosRenderizados = document.querySelectorAll(".palo")
+    palosRenderizados[0].innerHTML = (palos[azar]);
+    palosRenderizados[1].innerHTML = (palos[azar]);
+    document.querySelector(".numero").innerHTML = (numeros[azar2]);
+  });
+
 };
