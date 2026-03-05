2b060856f51a8f68bf5a6c503783f954c5ca140a
Ana Lopez <avantadesignlab@gmail.com>
2026-03-05 04:03:02 +0000

    dandole estilo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let palos = ["♦", "♥", "♠", "♣"]
+let numeros = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J", "A"];
+let azar = Math.floor(Math.random() * palos.length)
+let azar2 = Math.floor(Math.random() * numeros.length)
+
+
+  // document.querySelector("#card").innerHTML = (numeros[azar2]+ palos[azar]);
+  let palosRenderizados = document.querySelectorAll(".palo")
+  palosRenderizados[0].innerHTML = (palos[azar]);
+  palosRenderizados[1].innerHTML = (palos[azar]);
+  document.querySelector(".numero").innerHTML = (numeros[azar2]);
+
+    
+
+  
 };
