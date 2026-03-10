c5d9cf21796403aefa85709e6fdee44ac52b5bb3
Ana Lopez <avantadesignlab@gmail.com>
2026-03-10 15:42:31 +0000

    refactor change card size settings to apply on button click

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import "./assets/img/4geeks.ico";
 window.onload = function () {
   let palos = ["♦", "♥", "♠", "♣"]
   let numeros = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J", "A"];
-  
+
   // document.querySelector("#card").innerHTML = (numeros[azar2]+ palos[azar]);
   let palosRenderizados = document.querySelectorAll(".palo")
   let boton = document.querySelector(".updatecard");
@@ -19,14 +19,14 @@ window.onload = function () {
 
   function generarCarta() {
 
-  let azar = Math.floor(Math.random() * palos.length)
-  let azar2 = Math.floor(Math.random() * numeros.length)
+    let azar = Math.floor(Math.random() * palos.length)
+    let azar2 = Math.floor(Math.random() * numeros.length)
 
-  palosRenderizados[0].innerHTML = (palos[azar]);
-  palosRenderizados[1].innerHTML = (palos[azar]);
-  document.querySelector(".numero").innerHTML = (numeros[azar2]);
+    palosRenderizados[0].innerHTML = (palos[azar]);
+    palosRenderizados[1].innerHTML = (palos[azar]);
+    document.querySelector(".numero").innerHTML = (numeros[azar2]);
 
-  if (palos[azar] === "♥" || palos[azar] === "♦") {
+    if (palos[azar] === "♥" || palos[azar] === "♦") {
       palosRenderizados[0].style.color = "red"
       palosRenderizados[1].style.color = "red"
     } else {
@@ -36,27 +36,43 @@ window.onload = function () {
 
   }
 
-   generarCarta();
+  generarCarta();
 
-   boton.addEventListener("click", generarCarta)
+  boton.addEventListener("click", generarCarta)
 
-   let inputHeight = document.getElementById("heightInput");
-   let inputWidth = document.getElementById("widthInput");
-   let tarjeta = document.querySelector(".tarjeta");
+  let inputHeight = document.getElementById("heightInput");
+  let inputWidth = document.getElementById("widthInput");
+  let tarjeta = document.querySelector(".tarjeta");
+  let botonSize = document.getElementById("applySize");
 
-  inputHeight.addEventListener("input", function (event){
-  
-  let height = event.target.value;
-  tarjeta.style.height = height + "px";
+  botonSize.addEventListener("click", function () {
 
-  });
+    let height = inputHeight.value;
+    let width = inputWidth.value;
 
-  inputWidth.addEventListener("input", function (event){
+    tarjeta.style.height = height + "px";
+    tarjeta.style.width = width + "px";
 
-  let width = event.target.value;
-  tarjeta.style.width = width + "px";
-  
-});
 
+  })
+
+  setInterval(function () {
+    location.reload();
+  }, 20000);
+
+  // inputHeight.addEventListener("input", function (event){
+
+  // let height = event.target.value;
+  // tarjeta.style.height = height + "px";
+
+  // });
+
+  // inputWidth.addEventListener("input", function (event){
+
+  // let width = event.target.value;
+  // tarjeta.style.width = width + "px";
 
 };
+
+
+
